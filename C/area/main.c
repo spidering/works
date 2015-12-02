30461d2ae7967cf7ebcca7dcb6e859ca6abd8cff
spidering <newstedster@gmail.com>
2015-12-02 15:15:35 +0900

    C

---
+++ C/area/main.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main() {
+int a;
+int b;
+
+
+printf("height=");
+scanf("%d",&a);
+printf("width=");
+scanf("%d",&b);
+printf("The area of rectangular is %d square metre \n", a * b);
+}
---
+++ C/calculate/main.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main() {
+    int a;
+	int b;
+	printf("a=");
+	scanf("%d",&a);
+	printf("b=");
+	scanf("%d",&b);
+	printf("a + b = %d\n",a + b);
+	printf("a - b = %d\n",a - b);
+	printf("a * b = %d\n",a * b);
+	printf("a / b = %d\n",a / b);
+	printf("a %% b is %d\n",a % b);
+}
